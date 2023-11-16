8de9df4207fcc6951eb8861d91a764296d2cae84
nezba00 <nbahou98@gmail.com>
2023-11-16 13:32:17 +0100

    Bug fix --> path to genome index directory was wrong

--- hisat_alignment.sh
+++ hisat_alignment.sh
@@ -15,11 +15,11 @@ references_path=/data/courses/rnaseq_course/lncRNAs/fastq
 # Alignment for test strains
 # --phred33 NOt sure if default but better to specify
 # -p 8 to make use of all 8 cores
-hisat2 --phred33 -p 8 -x $home_path/Reference_index -1 $references_path/1_1_L3_R1_001_ij43KLkHk1vK.fastq.gz  -2 $references_path/1_1_L3_R2_001_qyjToP2TB6N7.fastq.gz -S $home_path/SAM/1_1.sam
-hisat2 --phred33 -p 8 -x $home_path/Reference_index -1 $references_path/1_2_L3_R1_001_DnNWKUYhfc9S.fastq.gz -2 $references_path/1_2_L3_R2_001_SNLaVsTQ6pwl.fastq.gz -S $home_path/SAM/1_2.sam
-hisat2 --phred33 -p 8 -x $home_path/Reference_index -1 $references_path/1_5_L3_R1_001_iXvvRzwmFxF3.fastq.gz  -2 $references_path/1_5_L3_R2_001_iXCMrktKyEh0.fastq.gz -S $home_path/SAM/1_3.sam
+hisat2 --phred33 -p 8 -x $home_path/Reference_index/GRCh38 -1 $references_path/1_1_L3_R1_001_ij43KLkHk1vK.fastq.gz  -2 $references_path/1_1_L3_R2_001_qyjToP2TB6N7.fastq.gz -S $home_path/SAM/1_1.sam
+hisat2 --phred33 -p 8 -x $home_path/Reference_index/GRCh38 -1 $references_path/1_2_L3_R1_001_DnNWKUYhfc9S.fastq.gz -2 $references_path/1_2_L3_R2_001_SNLaVsTQ6pwl.fastq.gz -S $home_path/SAM/1_2.sam
+hisat2 --phred33 -p 8 -x $home_path/Reference_index/GRCh38 -1 $references_path/1_5_L3_R1_001_iXvvRzwmFxF3.fastq.gz  -2 $references_path/1_5_L3_R2_001_iXCMrktKyEh0.fastq.gz -S $home_path/SAM/1_3.sam
 
 # Alignment for parental strains
-hisat2 --phred33 -p 8 -x $home_path/Reference_index -1 $references_path/P1_L3_R1_001_9L0tZ86sF4p8.fastq.gz  -2 $references_path/P1_L3_R2_001_yd9NfV9WdvvL.fastq.gz -S $home_path/SAM/P_1.sam
-hisat2 --phred33 -p 8 -x $home_path/Reference_index -1 $references_path/P2_L3_R1_001_R82RphLQ2938.fastq.gz  -2 $references_path/P2_L3_R2_001_06FRMIIGwpH6.fastq.gz -S $home_path/SAM/P_2.sam
-hisat2 --phred33 -p 8 -x $home_path/Reference_index -1 $references_path/P3_L3_R1_001_fjv6hlbFgCST.fastq.gz  -2 $references_path/P3_L3_R2_001_xo7RBLLYYqeu.fastq.gz -S $home_path/SAM/P_3.sam
+hisat2 --phred33 -p 8 -x $home_path/Reference_index/GRCh38 -1 $references_path/P1_L3_R1_001_9L0tZ86sF4p8.fastq.gz  -2 $references_path/P1_L3_R2_001_yd9NfV9WdvvL.fastq.gz -S $home_path/SAM/P_1.sam
+hisat2 --phred33 -p 8 -x $home_path/Reference_index/GRCh38 -1 $references_path/P2_L3_R1_001_R82RphLQ2938.fastq.gz  -2 $references_path/P2_L3_R2_001_06FRMIIGwpH6.fastq.gz -S $home_path/SAM/P_2.sam
+hisat2 --phred33 -p 8 -x $home_path/Reference_index/GRCh38 -1 $references_path/P3_L3_R1_001_fjv6hlbFgCST.fastq.gz  -2 $references_path/P3_L3_R2_001_xo7RBLLYYqeu.fastq.gz -S $home_path/SAM/P_3.sam

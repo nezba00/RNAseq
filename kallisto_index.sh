2b60be5d300313d323e883aad1f7852968551226
nezba00 <nbahou98@gmail.com>
2023-11-27 11:43:05 +0100

    Created kallisto_index
    Restored stringtie_Script

---
+++ kallisto_index.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=6G
+#SBATCH --job-name="Kall_Index"
+#SBATCH --time=05:00:00
+#SBATCH --output=kallIndex_%j.out
+#SBATCH --error=kallIndex_%j.err
+
+# Load Kallisto module
+module load kallisto/0.48.0-gompi-2021a
+
+# Define path variables
+home=/data/users/bnezar/RNA_Project
+reference=/data/courses/rnaseq_course/lncRNAs/Project1/references
+
+#Create Kallisto directory
+mkdir kallisto
+
+# Run kallisto index on reference file
+kallisto index --index="$home/kallisto/GRCh38_kallisto_index" $reference/GRCh38.genome.fa
+

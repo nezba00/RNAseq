9b2588493898cc0628047ce580e58593f941b3c0
nezba00 <nbahou98@gmail.com>
2023-11-28 13:52:34 +0100

    Make a new directory for the quant output modified path to fastq files

--- kallisto_quant.sh
+++ kallisto_quant.sh
@@ -11,7 +11,10 @@
 module load UHTS/Analysis/kallisto/0.46.0
 
 home=/data/users/bnezar/RNA_Project
-reference=/data/courses/rnaseq_course/lncRNAs/Project1/references
+fastq=/data/courses/rnaseq_course/lncRNAs/fastq
+
+# make a new directory for the quantification data
+mkdir $home/kallisto/quant
 
 # Run Kallisto quant
 # Args:
@@ -22,12 +25,12 @@ reference=/data/courses/rnaseq_course/lncRNAs/Project1/references
 # Usage: kallisto quant [arguments] FASTQ-files
 kallisto quant -t 8 --rf-stranded \
                 -i $home/kallisto/GRCh38_kallisto_index \
-                -o $home/kallisto \
-                "1_1_L3_R1_001_ij43KLkHk1vK.fastq.gz" "1_1_L3_R2_001_qyjToP2TB6N7.fastq.gz" \
-                "1_2_L3_R1_001_DnNWKUYhfc9S.fastq.gz" "1_2_L3_R2_001_SNLaVsTQ6pwl.fastq.gz \
-                "1_5_L3_R1_001_iXvvRzwmFxF3.fastq.gz" "1_5_L3_R2_001_iXCMrktKyEh0.fastq.gz \
-                "P1_L3_R1_001_9L0tZ86sF4p8.fastq.gz" "P1_L3_R2_001_yd9NfV9WdvvL.fastq.gz" \
-                "P2_L3_R1_001_R82RphLQ2938.fastq.gz" "P2_L3_R2_001_06FRMIIGwpH6.fastq.gz" \
-                "P3_L3_R1_001_fjv6hlbFgCST.fastq.gz" "P3_L3_R2_001_xo7RBLLYYqeu.fastq.gz"
+                -o $home/kallisto/quant \
+                 $fastq/1_1_L3_R1_001_ij43KLkHk1vK.fastq.gz $fastq/1_1_L3_R2_001_qyjToP2TB6N7.fastq.gz \
+                 $fastq/1_2_L3_R1_001_DnNWKUYhfc9S.fastq.gz $fastq/1_2_L3_R2_001_SNLaVsTQ6pwl.fastq.gz \
+                 $fastq/1_5_L3_R1_001_iXvvRzwmFxF3.fastq.gz $fastq/1_5_L3_R2_001_iXCMrktKyEh0.fastq.gz \
+                 $fastq/P1_L3_R1_001_9L0tZ86sF4p8.fastq.gz $fastq/P1_L3_R2_001_yd9NfV9WdvvL.fastq.gz \
+                 $fastq/P2_L3_R1_001_R82RphLQ2938.fastq.gz $fastq/P2_L3_R2_001_06FRMIIGwpH6.fastq.gz \
+                 $fastq/P3_L3_R1_001_fjv6hlbFgCST.fastq.gz $fastq/P3_L3_R2_001_xo7RBLLYYqeu.fastq.gz
 
 

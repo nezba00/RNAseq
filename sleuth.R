fcc993efdbbb2b45bdd71d7b096455b1bd0331be
nezba00 <119873345+nezba00@users.noreply.github.com>
2024-01-23 13:52:00 +0100

    Add files via upload
    --> Added Biotypes for target mapping
    --> Added code to create a BED file for import to the cluster

--- sleuth.R
+++ sleuth.R
@@ -6,6 +6,8 @@ BiocManager::install("rhdf5")
 library("rhdf5")
 BiocManager::install("EnhancedVolcano")
 library(EnhancedVolcano)
+BiocManager::install("biomaRt")
+library(biomaRt)
 
 
 # Install sleuth
@@ -22,6 +24,11 @@ library(cowplot)
 # Load dplyer
 install.packages("dplyr")
 library(dplyr)
+
+library(tidyr)
+
+# set working directory
+setwd("C:/Users/nbaho/OneDrive/Desktop/Bioinf/R-studio")
 
 # Get name of all kallisto dirs
 id <- dir(file.path("..", "RNA_Seq_Project", "Kallisto_Data" ))
@@ -42,8 +49,30 @@ ref_df <- data.frame(sample = id,condition = c("parental", "parental", "parental
 ref_df$path <- kallisto_dirs
 
 
+### Get Biotypes and make target mapping
+# biomaRt::useMart() to connect to specified biomart database
+mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "ensembl.org")
+
+# Show possible attributes for getBM
+attributes = listAttributes(mart)
+
+# get information for sleuth table in the end
+mapping <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id",
+                                     "external_gene_name", "gene_biotype",
+                                     "transcript_biotype"), mart = mart)
+
+# For target mapping in sleuth we need at least one column "target id"!
+mapping <- dplyr::rename(mapping, target_id = ensembl_transcript_id,
+                     ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
+###building sleuth object
+##so object contains info about experiment and details of the model for DE testing
+#loading kallisto data into the so object
+
+
+
+
 # Initialize Sleuth Object
-sleuth_object <- sleuth_prep(ref_df, extra_bootstrap_summary = TRUE)
+sleuth_object <- sleuth_prep(ref_df, extra_bootstrap_summary = TRUE, target_mapping = mapping)
 
 
 # Fit full model
@@ -67,30 +96,111 @@ sleuth_object <- sleuth_fit(sleuth_object, ~1, 'reduced')
 
 
 
-# Perform Sleuth test
-sleuth_object <- sleuth_lrt(sleuth_object, 'reduced', 'full')
-
+# Perform Sleuth test XXX --> we should do it with wt
+# sleuth_object <- sleuth_lrt(sleuth_object, 'reduced', 'full')
+sleuth_object <- sleuth_wt(sleuth_object, "conditionparental")
 
 # Read sleuth data into a table
 # Code from pachterlab
-sleuth_table <- sleuth_results(sleuth_object, 'reduced:full', 'lrt', show_all = FALSE)
+sleuth_table <- sleuth_results(sleuth_object, 'conditionparental', 'wt', show_all = FALSE)
 sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05)
-head(sleuth_significant, 20)
+head(sleuth_significant, 40)
 
 
 # Plot the data
 plot_bootstrap(sleuth_object, "ENST00000371817.8", units = "est_counts", color_by = "condition")
 
 # Volcano plot with Enhanced Volcano
-EnhancedVolcano()
+#EnhancedVolcano()
 
 
 # Volcano plot with sleuth 
-plot_volcano(sleuth_object,"LRT" , test_type = "wt", which_model = "full",
+plot_volcano(sleuth_object, test = "conditionparental", test_type = "wt", which_model = "full",
              sig_level = 0.1, point_alpha = 0.2, sig_color = "red",
              highlight = NULL)
 
 
+
+
+
+# Create New dataframe with necessary information for bedfile
+lnc_RNA_data <- dplyr::filter(sleuth_significant, gene_biotype == "lncRNA")
+novel_transcripts <- dplyr::filter(sleuth_significant, is.na(ens_gene))
+
+
+dif_exp_results <- sleuth_significant$ 
+dplyr::filter(gtf_df,  )
+
+
+# Make 2 bed files
+# --> one with ENST
+# --> one with novel transcripts
+
+no_exon_gtf <- filter(gtf_df, V3 == "transcript")
+# select columns: CHR, star, end, transcript id, strand
+bed_precursor <- select(no_exon_gtf, "V1","V3","V4","V5","V7","V9")
+# Split last column with many attributes separated by ";"
+bed_precursor_split <- separate_wider_delim(bed_precursor, V9, ";", names = c("gene_id","transcript_id"), too_many = "merge")
+pre_bed_split_transcripts <- separate_wider_delim(bed_precursor_split, transcript_id, ";", names = c("transcript_id", "rest"), too_many = "merge")
+pre_bed_split_transcripts <- separate_wider_delim(pre_bed_split_transcripts, rest, ";", names = c("gene_name", "ref_gene_id"), too_many = "merge", too_few = "align_end")
+
+# rename other columns
+colnames(pre_bed_split_transcripts)[1] <- "Chr"
+colnames(pre_bed_split_transcripts)[3] <- "start"
+colnames(pre_bed_split_transcripts)[4] <- "end"
+colnames(pre_bed_split_transcripts)[5] <- "strand"
+
+
+# remove "gene_id" from gene_id column
+pre_bed_split_transcripts$gene_id <- gsub("gene_id ", "", pre_bed_split_transcripts$gene_id)
+# remove "transcript_id" from transcript_id column
+pre_bed_split_transcripts$transcript_id <- gsub("transcript_id ", "", pre_bed_split_transcripts$transcript_id)
+# remove "gene_name" from gene_name column
+pre_bed_split_transcripts$gene_name <- gsub("gene_name ", "", pre_bed_split_transcripts$gene_name)
+# remove "ref_gene_id" from ref_gene_id column
+pre_bed_split_transcripts$ref_gene_id <- gsub("ref_gene_id ", "", pre_bed_split_transcripts$ref_gene_id)
+pre_bed_split_transcripts$ref_gene_id <- gsub(";", "", pre_bed_split_transcripts$ref_gene_id)
+
+# split the dataframe in one for + stranded and one for - stranded
+plus_pre_bed <- pre_bed_split_transcripts[pre_bed_split_transcripts$strand == "+",]
+minus_pre_bed_inversed <- pre_bed_split_transcripts[pre_bed_split_transcripts$strand == "-",]
+
+# if we got transcripts on - strand we need to switch start and stop position
+# reorder columns in general
+minus_pre_bed <- minus_pre_bed_inversed[,c(1,4,3,7,5,6,8,9,2)]
+
+# Reorder Columns for plus_pre_bed
+plus_pre_bed <- plus_pre_bed[,c(1,3,4,7,5,6,8,9,2)]
+
+
+# Create a dataframe with only novel/known transcripts for + and - stranded transcripts
+plus_pre_bed_novel <- plus_pre_bed[is.na(plus_pre_bed$gene_name),]
+plus_pre_bed_known <- plus_pre_bed[!is.na(plus_pre_bed$gene_name),]
+
+minus_pre_bed_novel <- minus_pre_bed[is.na(minus_pre_bed$gene_name),]
+minus_pre_bed_known <- minus_pre_bed[!is.na(minus_pre_bed$gene_name),]
+
+# rename cols from minus pre bed for merging later
+#colnames(minus_pre_bed_novel)[colnames(minus_pre_bed_novel) == "start"] <- "stop"
+#colnames(minus_pre_bed_novel)[colnames(minus_pre_bed_novel) == "end"] <- "start"
+#colnames(minus_pre_bed_novel)[colnames(minus_pre_bed_novel) == "stop"] <- "end"
+
+
+
+
+# merge plus/minus dfs for each novel and known transcripts in a df
+full_bed_novel <- merge(plus_pre_bed_novel, minus_pre_bed_novel, all = T)
+full_bed_known <- merge(plus_pre_bed_known, minus_pre_bed_known, all = T)
+
+
+bed_path_novel <- "C:/Users/nbaho/OneDrive/Desktop/Bioinf/RNA_Seq_Project/novel.bed"
+bed_path_known <- "C:/Users/nbaho/OneDrive/Desktop/Bioinf/RNA_Seq_Project/known.bed"
+
+# save bed files for upload later
+write.table(full_bed_novel, file = bed_path_novel, sep = "\t", quote = FALSE,
+            col.names = FALSE, row.names = FALSE)
+write.table(full_bed_known, file = bed_path_known, sep = "\t", quote = FALSE,
+            col.names = FALSE, row.names = FALSE)
 
 
 
@@ -108,3 +218,6 @@ plot_volcano(sleuth_object,"LRT" , test_type = "wt", which_model = "full",
 
 # t2g target mappimg
 
+
+loaded_packages <- installed.packages()[, "Package"]
+print(loaded_packages)

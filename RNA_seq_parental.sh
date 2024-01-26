55b09cf90105f049a381ea78962850a63e7764f3
nezba00 <nbahou98@gmail.com>
2024-01-26 15:09:07 +0100

    Added some comments

--- RNA_seq_parental.sh
+++ RNA_seq_parental.sh
@@ -23,8 +23,8 @@ do
 done
 
 
-# Generate fastqcs of all sequences
-for file in "$FASTQdir"/P*.gz;
+# Generate fastqcs of all sequences. Changed "1*" to "P*" for parental strains
+for file in "$FASTQdir"/1*.gz;
 do
     fastqc -o "/data/users/bnezar/RNA_Project/FastQC_Files" "$file"
 done

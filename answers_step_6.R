74d84c82b5b21c5e024a2ce6b9f2edea3f35ae43
nezba00 <119873345+nezba00@users.noreply.github.com>
2024-01-29 18:47:05 +0100

    Multiple changes
    Added code to create a table with only lncRNAs
    
    Added unfinished code to save files

--- answers_step_6.R
+++ answers_step_6.R
@@ -5,7 +5,7 @@ install.packages("dplyr")
 library(dplyr)
 
 
-## Read bedfiles
+## Read bedfiles from evaluation on cluster
 # these were generated on the cluster --> step_6/output_files
 intersect_data <- read.table("C:/Users/nbaho/OneDrive/Desktop/Bioinf/RNA_Seq_Project/intersect.bed", 
                              sep = "\t")
@@ -22,12 +22,24 @@ transcript_data <- read.table("C:/Users/nbaho/OneDrive/Desktop/Bioinf/RNA_Seq_Pr
                               sep = "\t", header = T)
 
 
+# Filter cpat output for transcripts with coding prob lower than 0.364 --> recommended on cpat doc page
+filtered_cpat <- filter(cpat_data, coding_prob < 0.364)
+
+
+
+
+
+
+
+
 ## Create summary table for novel transcripts
 novel_light_bed$intergenic <- NA
 novel_light_bed$tss <- NA
 novel_light_bed$poly_a <- NA
 novel_light_bed$tss_poly_a <- NA
+novel_light_bed$protein_potential_bool <- NA
 novel_light_bed$protein_potential <- NA
+
 
 
 # Add tss info
@@ -52,37 +64,56 @@ for (i in 1:length(poly_a_data$V4)){
 novel_light_bed$tss_poly_a[novel_light_bed$tss == novel_light_bed$poly_a] <- T
 
 
+# Add Info about coding probability
+novel_light_bed$protein_potential <- cpat_data$coding_prob
+
+for (i in 1:length(novel_light_bed$V1)){
+  if (novel_light_bed$protein_potential[i] < 0.364){
+    novel_light_bed$protein_potential_bool[i] <- T
+  }
+}
+
+
 
 # Summary for novel transcripts
 correct_tss <- sum(novel_light_bed$tss, na.rm = T)
 correct_poly_a <- sum(novel_light_bed$poly_a, na.rm = T)
 correct_5_3 <- sum(novel_light_bed$tss_poly_a, na.rm = T)
 intergenic_transcripts <- sum(novel_light_bed$intergenic, na.rm = T)
+is_protein_coding <- sum(is.na(novel_light_bed$protein_potential_bool))
 total_novel <- length(novel_light_bed$V4)
 
 # Calculate Ratios when compared to novel
 tss_ratio <- correct_tss/total_novel
 poly_a_ratio <- correct_poly_a/total_novel
 corr_5_3_ratio <- correct_5_3/total_novel
-intergenic_ratio <- intergenic_transcripts/total_novel 
+intergenic_ratio <- intergenic_transcripts/total_novel
+is_protein_coding_ratio <- is_protein_coding/total_novel
 
 
-# Create a table with all results and ratios --> still need to add coding potential
+# Create a table with all results and ratios for step 6
 result_table <- data.frame("Category" = c("Correct TSS", "Correct Poly-A", "Correct TSS & Poly-A",
-                                          "Intergenic Transcripts", "Total Novel Transcripts"),
+                                          "Intergenic Transcripts", "Protein Coding Transcripts", 
+                                          "Total Novel Transcripts"),
                            "Number" = c(correct_tss, correct_poly_a, correct_5_3,
-                                        intergenic_transcripts, total_novel),
+                                        intergenic_transcripts, is_protein_coding, total_novel),
                            "Ratio of Novel" = c(tss_ratio, poly_a_ratio, corr_5_3_ratio,
-                                                intergenic_ratio, 1))
+                                                intergenic_ratio, is_protein_coding_ratio, 1))
+
+
+
+
+
 
 
 
 ## Create summary table for significant transcripts
 # Create new cols
-transcript_data$intergenic <- NA
-transcript_data$tss <- NA
-transcript_data$poly_a <- NA
-transcript_data$tss_poly_a <- NA
+transcript_data$intergenic <- F
+transcript_data$tss <- F
+transcript_data$poly_a <- F
+transcript_data$tss_poly_a <- F
+transcript_data$protein_potential_bool <- F
 transcript_data$protein_potential <- NA
 
 # Add tss info
@@ -103,8 +134,29 @@ for (i in 1:length(poly_a_data$V4)){
   transcript_data$poly_a[target_index] <- T
 }
 
+
+# Add Info about coding probability
+for (i in 1:length(novel_light_bed$V1)){
+  if (!is.na(novel_light_bed$protein_potential_bool[i])){
+    target_index <- which(trimws(transcript_data$target_id, "l") ==  trimws(novel_light_bed$V4[i], "l"))
+    transcript_data$protein_potential_bool[target_index] <- T
+  }
+}
+
+for (i in 1:length(transcript_data$target_id)){
+  if (is.na(transcript_data$ens_gene[i])){
+    target_index <- which(trimws(novel_light_bed$V4, "l") == trimws(transcript_data$target_id[i], "l"))
+    if (!length(target_index) == 0){
+      transcript_data$protein_potential[i] <- novel_light_bed$protein_potential[target_index]
+    }
+    else {
+      next
+    }
+  }
+}
+
 # Add info if both poly A AND tss are True
-transcript_data$tss_poly_a[transcript_data$tss == transcript_data$poly_a] <- T
+transcript_data$tss_poly_a[transcript_data$tss & transcript_data$poly_a] <- T
 
 
 
@@ -116,25 +168,87 @@ num_tss_polyA_sig <- sum(transcript_data$tss_poly_a, na.rm = T)
 num_intergenic_sig <- sum(transcript_data$intergenic, na.rm = T)
 total_significant <- length(transcript_data$target_id)
 novel_significant <- sum(grepl("^MSTRG", transcript_data$target_id))
+num_protein_coding <- novel_significant - sum(transcript_data$protein_potential_bool, na.rm = T)
 
 # Calculate Ratios when compared to significant transcripts
 tss_ratio_sig <- num_tss_sig/novel_significant
 poly_a_ratio_sig <- num_polyA_sig/novel_significant
 corr_5_3_ratio_sig <- num_tss_polyA_sig/novel_significant
 intergenic_ratio_sig <- num_intergenic_sig/novel_significant
+protein_coding_ratio <- num_protein_coding/novel_significant
 
 # Create a table with all results and ratios --> still need to add coding potential
 sig_result_table <- data.frame("Number" = c(num_tss_sig, num_polyA_sig, num_tss_polyA_sig,
-                                 num_intergenic_sig, novel_significant),
+                                 num_intergenic_sig, num_protein_coding, novel_significant),
                                 "Ratio of Novel Significant" = c(tss_ratio_sig, poly_a_ratio_sig, corr_5_3_ratio_sig,
-                                                      intergenic_ratio_sig, 1))
+                                                      intergenic_ratio_sig, protein_coding_ratio, 1))
 row.names(sig_result_table) <- c("Correct TSS", "Correct Poly-A", "Correct TSS & Poly-A",
-                             "Intergenic Transcripts", "Total Novel Significant Transcripts")
+                             "Intergenic Transcripts", "Protein Coding Transcripts", 
+                             "Total Novel Significant Transcripts")
 
 
 
 
-## Create a table for significant lncRNAs
-sig_lnc_data <- filter(transcript_data, biotype == "lncRNA")
+
+
+
+
+## Create a ordered table for a scoring with novel significant genes
+ordered_results <- filter(transcript_data, grepl("^MST", target_id))
+
+# Calculate a score for transcripts based on correct 3', 5', intergenic and protein coding potential
+ordered_results$score <- 0
+
+for (i in 1:length(ordered_results$target_id)){
+  if (ordered_results$intergenic[i]){
+    ordered_results$score[i] = ordered_results$score[i] + 2
+  }
+  if (ordered_results$tss_poly_a[i]){
+    ordered_results$score[i] = ordered_results$score[i] + 3
+  }
+  if (ordered_results$protein_potential_bool[i]){
+    ordered_results$score[i] = ordered_results$score[i] + 1
+  }
+}
+
+
+# sort the ordered results
+sorted_results <- ordered_results[order(ordered_results$score, decreasing = T),]
+
+
+
+## Create a table for novel lncRNAs --> >200nt
+lnc_table <- novel_light_bed
+# Create column for orf length
+lnc_table$ORF_length <- lnc_table$V3 - lnc_table$V2
+
+
+lnc_table <- filter(lnc_table, ORF_length > 200, protein_potential < 0.364)
+
+
+## Create a table for significant lncRNAs --> >200nt
+lnc_table_sig <- transcript_data
+# look if a novel significant transcript can be found in lnc_table, if yes add biotype lncRNA
+for (i in 1:length(lnc_table_sig$biotype)){
+  if (trimws(lnc_table_sig$target_id[i], "l") %in% trimws(lnc_table$V4, "l"))
+    lnc_table_sig$biotype[i] <- "lncRNA"
+}
+
+# Filter for only lncRNAs
+lnc_table_sig <- filter(lnc_table_sig, biotype == "lncRNA")
+# Filter for novel
+lnc_table_sig_novel <- filter(lnc_table_sig, is.na(ens_gene))
+
+
+## Save all important files --> finish this tomorrow!
+# sig_genes_path <- "C:/Users/nbaho/OneDrive/Desktop/Bioinf/RNA_Seq_Project/sig_genes.tsv"
+# sig_transcripts_path <- "C:/Users/nbaho/OneDrive/Desktop/Bioinf/RNA_Seq_Project/sig_transcripts.tsv"
+# 
+# 
+# write.table(sig_genes_table, file = sig_genes_path, sep = "\t", quote = FALSE,
+#             col.names = TRUE, row.names = FALSE)
+# write.table(sig_transcripts_table, file = sig_transcripts_path, sep = "\t", quote = FALSE,
+#             col.names = TRUE, row.names = FALSE)
+
 
 
